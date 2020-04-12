d6a2cb2f7940e01cc2a9579c08f7e01b399d40f3
Alejandro Gonzalez <alejandrogonzalr@gmail.com>
2020-04-11 20:31:03 -0500

    Simple form UI

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div>
+      <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png">
+      </div>
+      <div class="col-6" style="margin: 20px auto">
+        <h1 align="center">Send your message</h1>
+        <ContactForm style="margin-top: 25px"></ContactForm>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import ContactForm from './components/ContactForm.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ContactForm
   }
 }
 </script>
---
+++ src/components/ContactForm.vue
@@ -0,0 +1,145 @@
+<template>
+  <div>
+    <b-form>
+      <b-form-group id="input-group-name" label="Name" label-for="input-name" required-input>
+        <b-form-input
+          id="input-name"
+          v-model="form.name"
+          name="your-name"
+          type="text"
+          v-validate="'required'"
+          placeholder="Enter your name">
+        </b-form-input>
+        <span v-show="errors.has('your-name')" v-html="requiredInput"></span>
+      </b-form-group>
+
+      <b-form-group id="input-group-email" label="Email" label-for="input-email" required>
+        <b-form-input
+          id="input-email"
+          v-model="form.email"
+          name="your-email"
+          type="email"
+          v-validate="'required'"
+          placeholder="Enter your email">
+        </b-form-input>
+        <span v-show="errors.has('your-email')" v-html="requiredInput"></span>
+      </b-form-group>
+
+      <b-form-group id="input-group-subject" label="Subject" label-for="input-subject" required>
+        <b-form-input
+          id="input-subject"
+          v-model="form.subject"
+          name="your-subject"
+          type="text"
+          v-validate="'required'"
+          placeholder="Enter your subject">
+        </b-form-input>
+        <span v-show="errors.has('your-subject')" v-html="requiredInput"></span>
+      </b-form-group>
+
+      <b-form-group id="input-group-message" label="Message" label-for="input-message" required>
+        <b-form-input
+          id="input-message"
+          v-model="form.message"
+          name="your-message"
+          type="text"
+          v-validate="'required'"
+          placeholder="Enter your message">
+        </b-form-input>
+        <span v-show="errors.has('your-message')" v-html="requiredInput"></span>
+      </b-form-group>
+
+      <section id="form-control">
+        <b-button id="submit" variant="success" @click="onSubmit" :disabled="isButtonDisabled">Send</b-button>
+        <b-button id="reset" variant="link" @click="onReset" style="margin-left: 5px" :disabled="isButtonDisabled">Reset</b-button>
+      </section>
+    </b-form>
+  </div>
+</template>
+
+<script>
+  import Vue from 'vue';
+  import VeeValidate from 'vee-validate';
+  import axios from 'axios';
+
+  Vue.use(VeeValidate);
+
+  const baseUrl = 'https://jsonplaceholder.typicode.com/posts'; // REST API test
+
+  export default {
+    name: "ContactForm",
+    data: () => {
+      return {
+        form: {
+          name: '',
+          email: '',
+          subject: '',
+          message: ''
+        },
+        isButtonDisabled: false,
+        requiredInput: 'This field is required'
+      }
+    },
+    methods: {
+      onSubmit(event) {
+        this.$validator.validateAll()
+          .then(valid => {
+            if (valid) {
+              this.isButtonDisabled = true;
+              this.postData();
+              event.preventDefault();
+            }
+          })
+      },
+      onReset(event) {
+        event.preventDefault();
+        for (let prop in this.form) {
+          this.form[prop] = '';
+        }
+      },
+      toast(variant, title, body) {
+        this.counter++;
+        this.$bvToast.toast(body, {
+          title: title,
+          variant: variant,
+          toaster: 'b-toaster-bottom-right',
+          solid: true,
+          autoHideDelay: 10000,
+          appendToast: false
+        })
+      },
+      postData() {
+        axios
+          .post(baseUrl, {
+            body: this.form
+          })
+          .then(response => {
+            this.isButtonDisabled = false;
+            this.toast('success', 'Process complete!', 'Message sent successfully');
+            // eslint-disable-next-line no-console
+            console.log(response)
+          })
+          .catch(err => {
+            this.toast('danger', 'Something has gone wrong!', 'The process could not be completed, please try again');
+            // eslint-disable-next-line no-console
+            console.log(err)
+          })
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  #form-control {
+    margin-top: 20px;
+    float: right;
+  }
+
+  #reset {
+    color: var(--danger);
+  }
+
+  span {
+    color: var(--danger);
+  }
+</style>
